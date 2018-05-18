0b474be58096c518c3fabcfb63dee28575014583
Saulo Carvalho <saulo.carv@hotmail.com>
2018-05-17 21:12:10 -0300

    Hello World

---
+++ MyFirstWebApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstWebApplication.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
